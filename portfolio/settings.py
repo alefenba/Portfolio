82c494f0a5f86c54dbba1bdfe66da9237dd9d53f
alefenba <alefe.182@outlook.com>
2022-04-06 20:48:13 -0300

    corrigindo dependencias

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIFILES_DIR= os.path.join(BASE_DIR, 'static')
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 COLLECTFAST_ENABLED = False
