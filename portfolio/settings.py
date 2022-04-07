04444b5922ea4150da6505199cddd1f1c0c6708e
alefenba <alefe.182@outlook.com>
2022-04-06 22:21:05 -0300

    retirando Sentry_dsn

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,22 +13,6 @@ import os
 import django_heroku
 from pathlib import Path
 from decouple import config
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    SENTRY_DSN=config('SENTRY_DSN'),
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
